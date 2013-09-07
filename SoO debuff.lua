bf5dfcdf9c4ccd34c820b400d7e282f37e321478
brain3r <raz0r.webber@gmail.com>
2013-09-08 00:54:36 +0300

    SoO Debuffs
    WOW 5.4

---
+++ SoO debuff.lua
@@ -0,0 +1,155 @@
+-- Siege of Orgrimmar
+
+		-- Immerseus
+		[SpellName(143297] = 5, -- Sha Splash
+		[SpellName(143459] = 5, -- Sha Resue
+		[SpellName(143524] = 4, -- Purified Resue
+		[SpellName(143286] = 4, -- Seeping Sha
+		[SpellName(143413] = 3, -- Swirl
+		[SpellName(143411] = 3, -- Swirl
+		[SpellName(143436] = 2, -- Corrosive Blast (tanks)
+		[SpellName(143579] = 3, -- Sha Corruption (Heroic Only)
+
+		-- Fallen Protectors
+		[SpellName(143239] = 4, -- Noxious Poison
+		[SpellName(144176] = 2, -- Lingering Anguish
+		[SpellName(143023] = 3, -- Corrupted Brew
+		[SpellName(143301] = 2, -- Gouge
+		[SpellName(143564] = 3, -- Meditative Field
+		[SpellName(143010] = 3, -- Corruptive Kick
+		[SpellName(143434] = 6, -- Shadow Word:Bane (Dispell)
+		[SpellName(143840] = 6, -- Mark of Anguish
+		[SpellName(143959] = 4, -- Defiled Ground
+		[SpellName(143423] = 6, -- Sha Sear
+		[SpellName(143292] = 5, -- Fixate
+		[SpellName(144176] = 5, -- Shadow Weakness
+		[SpellName(147383] = 4, -- Debilitation (Heroic Only)
+
+		-- Norushen
+		[SpellName(146124] = 2, -- Self Doubt (tanks)
+		[SpellName(146324] = 4, -- Jealousy
+		[SpellName(144639] = 6, -- Corruption
+		[SpellName(144850] = 5, -- Test of Reliance
+		[SpellName(145861] = 6, -- Self-Absorbed (Dispell)
+		[SpellName(144851] = 23, -- Test of Confidence (tanks)
+		[SpellName(146703] = 3, -- Bottomless Pit
+		[SpellName(144514] = 6, -- Lingering Corruption
+		[SpellName(144849] = 4, -- Test of Serenity
+
+		-- Sha of Pride
+		[SpellName(144358] = 2, -- Wounded Pre (tanks)
+		[SpellName(144843] = 3, -- Overcome
+		[SpellName(146594] = 4, -- Gift of the Titans
+		[SpellName(144351] = 6, -- Mark of Arrogance
+		[SpellName(144364] = 4, -- Power of the Titans
+		[SpellName(146822] = 6, -- Projection
+		[SpellName(146817] = 5, -- Aura of Pre
+		[SpellName(144774] = 2, -- Reaching Attacks (tanks)
+		[SpellName(144636] = 5, -- Corrupted Prison
+		[SpellName(144574] = 6, -- Corrupted Prison
+		[SpellName(145215] = 4, -- Banishment (Heroic)
+		[SpellName(147207] = 4, -- Weakened Resolve (Heroic)
+		[SpellName(144574] = 6, -- Corrupted Prison
+		[SpellName(144574] = 6, -- Corrupted Prison
+
+		-- Galakras
+		[SpellName(146765] = 5, -- Flame Arrows
+		[SpellName(147705] = 5, -- Poison Cloud
+		[SpellName(146902] = 2, -- Poison Tipped blades
+
+		-- Iron Juggernaut
+		[SpellName(144467] = 2, -- Ignite Armor
+		[SpellName(144459] = 5, -- Laser Burn
+		[SpellName(144498] = 5, -- Napalm Oil
+		[SpellName(144918] = 5, -- Cutter Laser
+		[SpellName(146325] = 6, -- Cutter Laser Target
+
+		-- Kor'kron Dark Shaman
+		[SpellName(144089] = 6, -- Toxic Mist
+		[SpellName(144215] = 2, -- Froststorm Strike (Tank only)
+		[SpellName(143990] = 2, -- Foul Geyser (Tank only)
+		[SpellName(144304] = 2, -- Rend 
+		[SpellName(144330] = 6, -- Iron Prison (Heroic)
+
+		-- General Nazgrim
+		[SpellName(143638] = 6, -- Bonecracker
+		[SpellName(143480] = 5, -- Assassin's Mark
+		[SpellName(143431] = 6, -- Magistrike (Dispell)
+		[SpellName(143494] = 2, -- Sundering Blow (Tanks) 
+		[SpellName(143882] = 5, -- Hunter's Mark
+
+		-- Malkorok
+		[SpellName(142990] = 2, -- Fatal Strike (Tank debuff)
+		[SpellName(142913] = 6, -- Displaced Energy (Dispell)
+		[SpellName(143919] = 5, -- Languish (Heroic)
+
+		-- Malkorok
+		[SpellName(145685] = 2, -- Unstable Defensive System
+		[SpellName(144853] = 3, -- Carnivorous Bite
+		[SpellName(145987] = 5, -- Set to Blow
+		[SpellName(145218] = 4, -- Harden Flesh
+		[SpellName(145230] = 1, -- Forbidden Magic
+		[SpellName(146217] = 4, -- Keg Toss
+		[SpellName(146235] = 4, -- Breath of Fire
+		[SpellName(145523] = 4, -- Animated Strike
+		[SpellName(142983] = 6, -- Torment (the new Wrack)
+		[SpellName(145715] = 3, -- Blazing Charge
+		[SpellName(145747] = 5, -- Bubbling Amber
+		[SpellName(146289] = 4, -- Mass Paralysis
+
+		-- Thok the Bloodthirsty
+		[SpellName(143766] = 2, -- Panic (tanks)
+		[SpellName(143773] = 2, -- Freezing Breath (tanks)
+		[SpellName(143452] = 1, -- Bloodied
+		[SpellName(146589] = 5, -- Skeleton Key (tanks)
+		[SpellName(143445] = 6, -- Fixate
+		[SpellName(143791] = 5, -- Corrosive Blood
+		[SpellName(143777] = 3, -- Frozen Sol (tanks)
+		[SpellName(143780] = 4, -- Ac Breath
+		[SpellName(143800] = 5, -- Icy Blood
+		[SpellName(143428] = 4, -- Tail Lash
+
+		-- Siegecrafter Blackfuse
+		[SpellName(144236] = 4, -- Pattern Recognition
+		[SpellName(144466] = 5, -- Magnetic Crush
+		[SpellName(143385] = 2, -- Electrostatic Charge (tank)
+		[SpellName(143856] = 6, -- Superheated
+
+		-- Paragons of the Klaxxi
+		[SpellName(143617] = 5, -- Blue Bomb
+		[SpellName(143701] = 5, -- Whirling (stun)
+		[SpellName(143702] = 5, -- Whirling
+		[SpellName(142808] = 6, -- Fiery Edge
+		[SpellName(143609] = 5, -- Yellow Sword
+		[SpellName(143610] = 5, -- Red Drum
+		[SpellName(142931] = 2, -- Exposed Veins
+		[SpellName(143619] = 5, -- Yellow Bomb
+		[SpellName(143735] = 6, -- Caustic Amber
+		[SpellName(146452] = 5, -- Resonating Amber
+		[SpellName(142929] = 2, -- Tenderizing Strikes
+		[SpellName(142797] = 5, -- Noxious Vapors
+		[SpellName(143939] = 5, -- Gouge
+		[SpellName(143275] = 2, -- Hewn
+		[SpellName(143768] = 2, -- Sonic Projection
+		[SpellName(142532] = 6, -- Toxin: Blue
+		[SpellName(142534] = 6, -- Toxin: Yellow
+		[SpellName(143279] = 2, -- Genetic Alteration
+		[SpellName(143339] = 6, -- Injection
+		[SpellName(142649] = 4, -- Devour
+		[SpellName(146556] = 6, -- Pierce
+		[SpellName(142671] = 6, -- Mesmerize
+		[SpellName(143979] = 2, -- Vicious Assault
+		[SpellName(143607] = 5, -- Blue Sword
+		[SpellName(143614] = 5, -- Yellow Drum
+		[SpellName(143612] = 5, -- Blue Drum
+		[SpellName(142533] = 6, -- Toxin: Red 
+		[SpellName(143615] = 5, -- Red Bomb
+		[SpellName(143974] = 2, -- Shield Bash (tanks)
+
+		-- Garrosh Hellscream
+		[SpellName(144582] = 4, -- Hamstring
+		[SpellName(144954] = 4, -- Realm of Y'Shaarj
+		[SpellName(145183] = 2, -- Gripping Despair (tanks)
+		[SpellName(144762] = 4, -- Desecrated
+		[SpellName(145071] = 5, -- Touch of Y'Sharrj
+		[SpellName(148718] = 4, -- Fire Pit	
